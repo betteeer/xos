f3ea3a7ed61affeec17e72992340ecf3a8c1510e
yanqingqing <qingqing.yan@inossem.com>
2023-05-24 16:44:53 +0800

    trigger webhook again

--- src/main/java/com/inossem/sco/common/core/exception/base/BaseException.java
+++ src/main/java/com/inossem/sco/common/core/exception/base/BaseException.java
@@ -1,6 +1,5 @@
 package com.inossem.sco.common.core.exception.base;
 
-
 public class BaseException extends RuntimeException {
     private static final long serialVersionUID = 1L;
     private String module;
