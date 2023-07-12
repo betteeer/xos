8e2181be0b514e0dea67de3f431c3aaf2a5c8662
ZhengLu <lu.zheng@inossem.com>
2023-07-12 16:40:10 +0800

    Business Partner页面：数据来源切换成调用bkp接口获取,oms数据库中business_partner表废弃

---
+++ src/main/java/com/inossem/oms/base/svc/domain/DTO/BkpAdditionalContactDto.java
@@ -0,0 +1,27 @@
+package com.inossem.oms.base.svc.domain.DTO;
+
+import io.swagger.annotations.ApiModel;
+import lombok.Data;
+
+/**
+ * @author zhenglu
+ * @date 2023/07/10
+ **/
+
+@Data
+@ApiModel("bkp_additional_contact对照实体")
+public class BkpAdditionalContactDto {
+
+    private Long id;
+
+    private String contact_type;
+
+    private String contact_name;
+
+    private String tel;
+
+    private String email;
+
+    private String note;
+
+}
---
+++ src/main/java/com/inossem/oms/base/svc/domain/DTO/BkpBusinessPartnerDto.java
@@ -0,0 +1,59 @@
+package com.inossem.oms.base.svc.domain.DTO;
+
+import io.swagger.annotations.ApiModel;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * @author zhenglu
+ * @date 2023/07/10
+ **/
+
+@Data
+@ApiModel("bkp_bp对照实体")
+public class BkpBusinessPartnerDto {
+
+    private Long id;
+
+    private Long company_id;
+
+    private Integer company_code;
+
+    private String contact_name;
+
+    private String gl_account;
+
+    private String tel;
+
+    private String email;
+
+    private String office_receiver;
+
+    private String contact_id;
+
+    private String office_street;
+
+    private String office_city;
+
+    private String office_province;
+
+    private String office_country;
+
+    private String office_postal_code;
+
+    private String billing_street;
+
+    private String billing_city;
+
+    private String billing_province;
+
+    private String billing_country;
+
+    private String billing_postal_code;
+
+    private List<BkpAdditionalContactDto> additional_contact;
+
+    private List<BkpShippingAddressDto> shipping_address;
+
+}
